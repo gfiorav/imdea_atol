8a4adcf3d27296ff9f18fd703c7763ac38f630e8
gfiorav <guido.fioravantti@gmail.com>
2014-04-01 11:23:36 +0200

    Adds performance tweak. Now the logger check for total amount of packets sent since last iteration before doing any further operations

--- src/com/imdea/networks/apol/ApplicationLogger.java
+++ src/com/imdea/networks/apol/ApplicationLogger.java
@@ -26,9 +26,11 @@ public class ApplicationLogger {
 	private long prevRxBytes [] = new long [this.appsInfo.size()];
 	private long prevTxPackets [] = new long [this.appsInfo.size()];
 	private long prevRxPackets [] = new long [this.appsInfo.size()];
+	private long prevTotalRxPackets;
+	private long prevTotalTxPackets;
 
 	private Timer timer;
-	
+
 	private String path = "sdcard/Download/imdea_atol.xml";
 
 
@@ -47,6 +49,9 @@ public class ApplicationLogger {
 			this.prevTxPackets[i] = TrafficStats.getUidTxPackets(ai.uid);
 			i++;
 		}
+
+		prevTotalRxPackets = TrafficStats.getTotalRxPackets();
+		prevTotalTxPackets = TrafficStats.getTotalTxPackets();
 
 	}
 
@@ -70,91 +75,100 @@ public class ApplicationLogger {
 
 	@SuppressLint("NewApi")
 	public void logActivity () {
-		try {
-			boolean appending = false;
-			
-			// Signal if file exists, we don't declare xml header
-			RandomAccessFile rof = new RandomAccessFile(path, "rw");
-			
-			if(rof.length() > 0)
-			{
-				appending = true;
-			}
+		// Check if any new data was registered before logging
+		long currentTotalRxPackets = TrafficStats.getTotalRxPackets();
+		long currentTotalTxPackets = TrafficStats.getTotalTxPackets();
 
-			// If file existed, start before closing tag </events>, else declare header
-			if(appending) {
-				rof.seek(rof.length() -("</events>".length() +1));
-			} else {
-				rof.writeBytes("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone= \"yes\"?>\n");
-				rof.writeBytes("<events>\n");
-			}
+		if((currentTotalRxPackets - this.prevTotalRxPackets) > 0 || (currentTotalTxPackets - this.prevTotalTxPackets) > 0) {
+			try {
+				boolean appending = false;
 
-			Calendar c = Calendar.getInstance(); 
-			int millisecond 	= c.get(Calendar.MILLISECOND);
-			int second 			= c.get(Calendar.SECOND);
-			int minute 			= c.get(Calendar.MINUTE);
-			int hour			= c.get(Calendar.HOUR);
-			int day				= c.get(Calendar.DAY_OF_MONTH);
-			int month 			= c.get(Calendar.MONTH);
-			int year 			= c.get(Calendar.YEAR);
+				// Signal if file exists, we don't declare xml header
+				RandomAccessFile rof = new RandomAccessFile(path, "rw");
 
-			int a = 0;
-			for(ApplicationInfo ai : this.appsInfo)
-			{
-
-				int uid = ai.uid;
-
-				long raw_rx_bytes = TrafficStats.getUidRxBytes(uid);
-				long raw_tx_bytes = TrafficStats.getUidTxBytes(uid);
-				long raw_rx_packets = TrafficStats.getUidRxPackets(uid);
-				long raw_tx_packets = TrafficStats.getUidTxPackets(uid);
-
-				long rx_bytes = raw_rx_bytes -this.prevRxBytes[a];
-				long tx_bytes = raw_tx_bytes -this.prevTxBytes[a];
-				long rx_packets = raw_rx_packets -this.prevRxPackets[a];
-				long tx_packets = raw_tx_packets -this.prevTxPackets[a];
-
-				if(rx_bytes != 0 || tx_bytes != 0) {
-					rof.write("\t<event>\n".getBytes());
-					
-					String package_name = ai.packageName;
-					rof.writeBytes("\t\t<time>\n");
-					rof.writeBytes("\t\t\t<day>" + day + "</day>" +"\n");
-					rof.writeBytes("\t\t\t<month>" + month + "</month>" +"\n");
-					rof.writeBytes("\t\t\t<year>" + year + "</year>" +"\n");
-					rof.writeBytes("\t\t\t<hour>" + hour + "</hour>" +"\n");
-					rof.writeBytes("\t\t\t<minute>" + minute + "</minute>" +"\n");
-					rof.writeBytes("\t\t\t<second>" + second + "</second>" +"\n");
-					rof.writeBytes("\t\t\t<millisecond>" + millisecond + "</millisecond>" +"\n");
-					rof.writeBytes("\t\t</time>\n");
-					rof.writeBytes("\t\t<app>" + package_name + "</app>" +"\n");
-					rof.writeBytes("\t\t<rx>"+"\n");
-					rof.writeBytes("\t\t\t<bytes>"+ rx_bytes +"</bytes>" +"\n");
-					rof.writeBytes("\t\t\t<packets>"+ rx_packets +"</packets>" +"\n");
-					rof.writeBytes("\t\t</rx>"+"\n");
-					rof.writeBytes("\t\t<tx>"+"\n");
-					rof.writeBytes("\t\t\t<bytes>"+ tx_bytes +"</bytes>" +"\n");
-					rof.writeBytes("\t\t\t<packets>"+ tx_packets +"</packets>" +"\n");
-					rof.writeBytes("\t\t</tx>"+"\n");
-					rof.writeBytes("\t</event>\n");
+				if(rof.length() > 0)
+				{
+					appending = true;
 				}
 
-				this.prevRxBytes[a] = raw_rx_bytes;
-				this.prevTxBytes[a] = raw_tx_bytes;
-				this.prevRxPackets[a] = raw_rx_packets;
-				this.prevTxPackets[a] = raw_tx_packets;
+				// If file existed, start before closing tag </events>, else declare header
+				if(appending) {
+					rof.seek(rof.length() -("</events>".length() +1));
+				} else {
+					rof.writeBytes("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone= \"yes\"?>\n");
+					rof.writeBytes("<events>\n");
+				}
 
-				a++;
+				Calendar c = Calendar.getInstance(); 
+				int millisecond 	= c.get(Calendar.MILLISECOND);
+				int second 			= c.get(Calendar.SECOND);
+				int minute 			= c.get(Calendar.MINUTE);
+				int hour			= c.get(Calendar.HOUR);
+				int day				= c.get(Calendar.DAY_OF_MONTH);
+				int month 			= c.get(Calendar.MONTH);
+				int year 			= c.get(Calendar.YEAR);
 
+				int a = 0;
+				for(ApplicationInfo ai : this.appsInfo)
+				{
+
+					int uid = ai.uid;
+
+					long raw_rx_bytes = TrafficStats.getUidRxBytes(uid);
+					long raw_tx_bytes = TrafficStats.getUidTxBytes(uid);
+					long raw_rx_packets = TrafficStats.getUidRxPackets(uid);
+					long raw_tx_packets = TrafficStats.getUidTxPackets(uid);
+
+					long rx_bytes = raw_rx_bytes -this.prevRxBytes[a];
+					long tx_bytes = raw_tx_bytes -this.prevTxBytes[a];
+					long rx_packets = raw_rx_packets -this.prevRxPackets[a];
+					long tx_packets = raw_tx_packets -this.prevTxPackets[a];
+
+					if(rx_bytes != 0 || tx_bytes != 0) {
+						rof.write("\t<event>\n".getBytes());
+
+						String package_name = ai.packageName;
+						rof.writeBytes("\t\t<time>\n");
+						rof.writeBytes("\t\t\t<day>" + day + "</day>" +"\n");
+						rof.writeBytes("\t\t\t<month>" + month + "</month>" +"\n");
+						rof.writeBytes("\t\t\t<year>" + year + "</year>" +"\n");
+						rof.writeBytes("\t\t\t<hour>" + hour + "</hour>" +"\n");
+						rof.writeBytes("\t\t\t<minute>" + minute + "</minute>" +"\n");
+						rof.writeBytes("\t\t\t<second>" + second + "</second>" +"\n");
+						rof.writeBytes("\t\t\t<millisecond>" + millisecond + "</millisecond>" +"\n");
+						rof.writeBytes("\t\t</time>\n");
+						rof.writeBytes("\t\t<app>" + package_name + "</app>" +"\n");
+						rof.writeBytes("\t\t<rx>"+"\n");
+						rof.writeBytes("\t\t\t<bytes>"+ rx_bytes +"</bytes>" +"\n");
+						rof.writeBytes("\t\t\t<packets>"+ rx_packets +"</packets>" +"\n");
+						rof.writeBytes("\t\t</rx>"+"\n");
+						rof.writeBytes("\t\t<tx>"+"\n");
+						rof.writeBytes("\t\t\t<bytes>"+ tx_bytes +"</bytes>" +"\n");
+						rof.writeBytes("\t\t\t<packets>"+ tx_packets +"</packets>" +"\n");
+						rof.writeBytes("\t\t</tx>"+"\n");
+						rof.writeBytes("\t</event>\n");
+					}
+
+					this.prevRxBytes[a] = raw_rx_bytes;
+					this.prevTxBytes[a] = raw_tx_bytes;
+					this.prevRxPackets[a] = raw_rx_packets;
+					this.prevTxPackets[a] = raw_tx_packets;
+
+					a++;
+
+				}
+
+				rof.writeBytes("</events>\n");
+
+				rof.close();
+			} catch (IOException ioe) {
+				// TODO Auto-generated catch block
+				ioe.printStackTrace();
 			}
-
-			rof.writeBytes("</events>\n");
-
-			rof.close();
-		} catch (IOException ioe) {
-			// TODO Auto-generated catch block
-			ioe.printStackTrace();
 		}
+		
+		this.prevTotalRxPackets = currentTotalRxPackets;
+		this.prevTotalTxPackets = currentTotalTxPackets;
 	}
 
 }
