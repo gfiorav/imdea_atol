4c33a6fb48a4ffafc1042491135b09b1e931b059
gfiorav <guido.fioravantti@gmail.com>
2014-07-09 18:50:12 +0200

    Discoverment works

--- src/com/imdea/networks/apol/UploadDB.java
+++ src/com/imdea/networks/apol/UploadDB.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import android.os.AsyncTask;
@@ -22,11 +25,33 @@ public class UploadDB extends AsyncTask<String, Void, String>{
 	private final String TICKI 		= "TICKI";
 	private final String TACKA 		= "TACKA";
 	private final String FFP 		= "FREEFORPASS";
+	private final String WFP		= "WAITINGFORPASS";
 	private final char SEP 			= ':';
 
 	public void upload() {
 		try {
-			InetAddress srvr_addr 	= InetAddress.getByName(Logger.db.address);
+			// Check for servers near by
+			DatagramSocket ds 		= new DatagramSocket(PORT + 1);
+			
+			int read;
+			byte [] buffer = new byte [BUFFER_SIZE];
+			
+			boolean discovered = false;
+			String received = null;
+			do {
+				DatagramPacket recv = new DatagramPacket(buffer, BUFFER_SIZE);
+				ds.receive(recv);
+				received = new String(recv.getData(), 0, recv.getLength());
+				
+				if(received.contains(WFP)) discovered = true;
+				
+			} while (!discovered);
+			
+			ds.close();
+			
+			int index = received.indexOf("@");
+			InetAddress srvr_addr = InetAddress.getByName(received.substring(index + 1, received.length()));
+
 			Socket sock 			= new Socket(srvr_addr, PORT);
 
 			File db_file 			= Logger.context.getDatabasePath(Logger.db.getDatabaseName());
@@ -34,12 +59,10 @@ public class UploadDB extends AsyncTask<String, Void, String>{
 			OutputStream os 		= sock.getOutputStream();
 			InputStream is 			= sock.getInputStream();
 
-			int read;
-			byte [] buffer = new byte [BUFFER_SIZE];
-
+			
 			// Wait server confirmation
 			read = is.read(buffer);
-			String received = new String(buffer);
+			received = new String(buffer);
 			if(!received.contains(FFP)) { this.cancel(true); }
 
 			// Send TICKI to server
@@ -72,8 +95,6 @@ public class UploadDB extends AsyncTask<String, Void, String>{
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-
 		}
 	}
 
